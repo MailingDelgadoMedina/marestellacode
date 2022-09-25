a66a15b5805fe1956e6769505bd7fb2f307e86e0
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-25 15:20:57 -0400

    setting up the env variables and adding domain on next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+  },
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
