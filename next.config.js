a7e581696c14428b4d6312b5f65ecd09b09ba94b
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-29 22:46:53 -0400

    removed the last yml and added config on next.config file to be able to build and export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   swcMinify: true,
   env: {
     SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN,
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
   },
   images: {
+    loader:"akamai",
+    path:"",
     domains: ["cdn.shopify.com"],
   },
 };
